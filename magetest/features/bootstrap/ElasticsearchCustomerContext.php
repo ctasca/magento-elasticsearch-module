0598e68f8ab7f61b5609601c7b2ff989ff9856ab
Carlo Tasca <carlo.tasca.mail@gmail.com>
2014-05-04 01:18:50 +0100

    added behat search scenarios for regression tests

--- magetest/features/bootstrap/ElasticsearchCustomerContext.php
+++ magetest/features/bootstrap/ElasticsearchCustomerContext.php
@@ -8,6 +8,8 @@ use MageTest\MagentoExtension\Context\MagentoContext;
 class ElasticsearchCustomerContext extends MyMagentoContext
 {
 
+    private $searchedTerm = false;
+
     /**
      * @Given /^I type "([^"]*)" in search input$/
      */
@@ -23,8 +25,9 @@ class ElasticsearchCustomerContext extends MyMagentoContext
     {
         $expected = $suggestions->getHash();
         foreach ($expected as $suggestion) {
-            $id = $suggestion['id'];
-            $isSuggestion = $this->getSession()->getDriver()->evaluateScript("$('elasticsearch-suggestion-$id').innerHTML");
+            $id           = $suggestion['id'];
+            $isSuggestion = $this->getSession()->getDriver()
+                                 ->evaluateScript("$('elasticsearch-suggestion-$id').innerHTML");
             if ($isSuggestion != $suggestion['suggestion'])
                 throw new Exception("Suggestion for '" . $suggestion['suggestion'] . "' not found in suggestions list");
         }
@@ -57,4 +60,88 @@ class ElasticsearchCustomerContext extends MyMagentoContext
         $suggestionElement = $this->_findElementWithXpath('//*[@id="elasticsearch-suggestion-' . $id . '"]');
         $suggestionElement[0]->click();
     }
+
+    /**
+     * @Given /^I search for "([^"]*)"$/
+     */
+    public function iSearchFor($text)
+    {
+        $this->getSession()->getPage()->fillField("search", $text);
+        $this->getSession()->getPage()->pressButton('Search');
+    }
+
+    /**
+     * @Given /^I should see the following layered navigation filters:$/
+     */
+    public function iShouldSeeTheFollowingLayeredNavigationFilters(TableNode $filters)
+    {
+        $expected = $filters->getHash();
+        foreach ($expected as $key => $filter) {
+            //*[@id="narrow-by-list"]/dt[1]
+            $iKey              = $key + 1;
+            $layeredFilter     = $this->_findElementWithXpath('//*[@id="narrow-by-list"]/dt[' . $iKey . ']');
+            $expectedFilerText = trim(strtoupper($filter['filter']));
+            $uFText            = trim(strtoupper($layeredFilter[0]->getText()));
+            if ($uFText != $expectedFilerText)
+                throw new \Exception("{$filter['filter']} filter element not found in layered naviation");
+        }
+    }
+
+    /**
+     * @Given /^I am seeing search results for "([^"]*)"$/
+     */
+    public function iAmSeeingSearchResultsFor($term)
+    {
+        $this->searchedTerm = $term;
+        $this->iShouldSee("Search results for '$term'");
+    }
+
+    /**
+     * @When /^I follow layered navigation Electronics filter$/
+     */
+    public function iFollowLayeredNavigationElectronicsFilter()
+    {
+        $layeredNavigationFilter = $this->_findElementWithXpath('//*[@id="narrow-by-list"]/dd[1]/ol/li/a');
+        $layeredNavigationFilter[0]->click();
+    }
+
+    /**
+     * @Given /^I should see the following filter "([^"]*)" links:$/
+     */
+    public function iShouldSeeTheFollowingFilterLinks($ddIndex, TableNode $table)
+    {
+        $this->_getLayeredNavFiltersByDdIndex($table, $ddIndex);
+    }
+
+
+    /**
+     * @param TableNode $filters
+     * @param           $ddIndex
+     * @throws Exception
+     */
+    protected function _getLayeredNavFiltersByDdIndex(TableNode $filters, $ddIndex)
+    {
+        $expected             = $filters->getHash();
+        $expectedResultsArray = array_fill(0, count($expected), true);
+        $expectedResults      = md5(serialize($expectedResultsArray));
+        $testResults          = array();
+        $notFound = array();
+        foreach ($expected as $i => $filter) {
+            $ii        = $i + 1;
+            $navFilter = $this->_findElementWithXpath('//*[@id="narrow-by-list"]/dd[' . $ddIndex . ']/ol/li[' . $ii . ']/a');
+            if ($filter['filter'] != trim($navFilter[0]->getText())) {
+                $notFound[$filter['filter']] = $filter['filter'];
+                continue;
+            }
+
+            $testResults[] = true;
+        }
+
+        $testResultsMd5 = md5(serialize($testResults));
+
+        if ($testResultsMd5 != $expectedResults)
+            throw new \Exception("One or more category filters were not found in layered naviation. Not found: " . implode(', ', $notFound));
+    }
+
+
 }
